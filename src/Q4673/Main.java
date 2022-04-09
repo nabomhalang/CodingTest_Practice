3b9de742d7d4d2359893580c04a55270536b4e53
나봄하랑 <21sunrin052@sunrint.hs.kr>
2022-04-10 00:13:49 +0900

    BOJ

---
+++ src/Q4673/Main.java
@@ -0,0 +1,29 @@
+package Q4673;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        boolean check[] = new boolean[10001];
+
+        for (int i = 0; i < 10001; i++) {
+            int n = SELF(i);
+            if ( n < 10001 ) check[n] = true;
+        }
+
+        for (int i = 1; i < 10001; i++) {
+            if (check[i] != true) bw.write(i + "\n");
+        }
+        bw.close();
+    }
+
+    private static int SELF(int n) {
+        int total = n;
+        while ( n != 0 ) {
+            total += ( n % 10 );
+            n /= 10;
+        }
+        return total;
+    }
+}
